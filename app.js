4214bad65ce60b5a524b61a991e04e4ce57eb796
Rika <rfnaufal@gmail.com>
2024-02-23 22:37:21 +0700

    Commit: task to use module, and routes

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-import express from  'express';
-const app = express();
-const port = 3000
+ import express from  'express';
+ import { router, users } from './routes/api/v1/index.js';
+ const app = express();
+ const port = 3000
 
-app.get('/',  (req, res) => {
-    res.send('Hello World!')
-})
+const routes = router;
+app.use('/api/v1', routes);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
 app.listen(port, () => {
-    console.log(`example app listening on port ${port}`)
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+router.get('/users/:id',  (req, res) => {
+    const id = req.params.id;
+    res.send(`Hello World! Name is ${users.name}. Nice to meet you!`);
+});
---
+++ routes/api/v1/index.js
@@ -0,0 +1,32 @@
+import express from  'express';
+const router = express.Router();
+const users = [
+        {id:1, name:"John"},
+        {id:2,name:"Jane"}
+    ];
+
+// Define routes
+router.get('/users', (req, res) => {
+  res.send('List of users');
+});
+
+router.get('/users/:id',  (req, res) => {
+    const id = req.params.id;
+    return res.send(`Hello World! Name is ${users[id].name}. Nice to meet you!`);
+});
+
+router.post('/users', (req, res) => {
+  res.send('Create a new user');
+});
+
+router.put('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`Update user ${userId}`);
+});
+
+router.delete('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`Delete user ${userId}`);
+});
+
+export { router, users };
